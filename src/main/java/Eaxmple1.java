63e8ef76218ac01d64c59e32ba30b35be44cb63a
Nilesh Pawar <nilesh.p.pawar@gmail.com>
2019-08-14 17:20:07 +0100

    Added two examples

---
+++ src/main/java/Eaxmple1.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Eaxmple1 {
+
+    /**
+     * Desc : This example will show how to print all elements using forEach with and without lambda
+     * @param args
+     */
+    public static void main(String[] args){
+
+        List<String> citiesLived = Arrays.asList("Pune","London", "Dublin", "JoBerg", "KL", "Machester");
+
+        // This will print all the element without lambda
+        citiesLived.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("City lived in without lambda " + s);
+            }
+        });
+
+        // This will print all the element with lambda
+        citiesLived.forEach(s -> System.out.println("City lived in with lambda " + s));
+
+        // This will print all the element with method reference. Please note in method reference parameter to lambda and method call is same
+        citiesLived.forEach(System.out::println);
+
+
+    }
+}
---
+++ src/main/java/Example2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Example2 {
+
+    /**
+     * Desc : Here we will cover printing all cities based on some conditions
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+
+        List<String> citiesLived = Arrays.asList("Pune","London", "Dublin", "JoBerg", "KL", "Machester");
+        System.out.println("Cities that starts with P");
+        printCities(citiesLived,s->s.startsWith("P"));
+
+        System.out.println("Cities that ends with r");
+        printCities(citiesLived,s->s.endsWith("r"));
+
+    }
+
+    public static void printCities(List<String> citiesLived, Predicate<String> predicate){
+        citiesLived.stream().filter(predicate).forEach(System.out::println);
+
+    }
+}
