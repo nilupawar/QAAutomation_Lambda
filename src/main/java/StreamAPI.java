6ed2ba21a4271864fb2b1718a9a15a64761769a4
Nilesh Pawar <nilesh.p.pawar@gmail.com>
2019-10-05 11:59:31 +0100

    Stream API Example

---
+++ src/main/java/StreamAPI.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+
+        String fullName = "Nilesh";
+        char[] nameArray = fullName.toCharArray();
+        String[] fullNames = {"Tulja","Supriya","Nilesh"};
+
+        fullName
+                .chars()
+                .mapToObj(a->(char)a)
+                .filter(a-> a.equals('i') || a.equals('h') )
+                .forEach(System.out::println);
+
+        Stream.of(nameArray)
+                .forEach(System.out::println);
+
+        Stream.of(fullNames)
+                .filter(a->a.equalsIgnoreCase("Tulja"))
+                .forEach(System.out::println);
+    }
+}
